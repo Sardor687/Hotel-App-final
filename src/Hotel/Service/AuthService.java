d40fdc9405f799aeffa29420dbb5f85fdcc4fc46
Dilshod <dilshodkarimov242@gmail.com>
2024-07-15 17:56:21 +0500

    Main, Authservise

--- src/Hotel/Service/AuthService.java
+++ src/Hotel/Service/AuthService.java
@@ -1,4 +1,74 @@
 package Hotel.Service;
 
+import Hotel.Database.Database;
+import Hotel.entities.Kard;
+import Hotel.entities.User;
+import Hotel.entities.Role.Role;
+
+import java.util.Scanner;
+
 public class AuthService {
+    public static Scanner scanner = new Scanner(System.in);
+    public static Scanner strScanner = new Scanner(System.in);
+    public static User Currentuser;
+
+    public static void Menyu() {
+        while (true) {
+            System.out.println("""
+                    0 exit
+                    1 sign in
+                    2 sign up
+                    """);
+            switch (scanner.nextInt()) {
+                case 0 -> {
+                    System.out.println("Bye!");
+                    return;
+                }
+                case 1 -> SignIn();
+                case 2 -> SignUp();
+            }
+        }
+    }
+
+    private static void SignUp() {
+        User user = new User();
+        System.out.println("Enter name: ");
+        String name=AuthService.strScanner.nextLine();
+        user.setName(name);
+        System.out.println("Enter password: ");
+        user.setPassword(AuthService.strScanner.nextLine());
+        System.out.println("Enter phone number: ");
+        user.setTel_number(AuthService.strScanner.nextLine());
+        user.setRole(Role.USER);
+        Kard kard = new Kard(null, null, null, 0.0);
+        user.setKard(kard);
+        Database.users.add(user);
+        System.out.println("successfully added user!");
+    }
+
+    private static void SignIn() {
+
+        System.out.println("Enter name: ");
+        String name = AuthService.strScanner.nextLine();
+        System.out.println("Enter password: ");
+        String password = AuthService.strScanner.nextLine();
+        boolean b = true;
+
+        for (User user : Database.users) {
+            if (user.getName().equals(name) && user.getPassword().equals(password) && user.getRole().equals(Role.USER)) {
+                Currentuser = user;
+                b = false;
+                UserService.UserMenyu();
+
+            }
+            if (user.getName().equals(name) && user.getPassword().equals(password) && user.getRole().equals(Role.ADMIN)) {
+                b = false;
+                AdminService.adminMenyu();
+            }
+        }
+
+        if (b) System.out.println("user not found!");
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
+import Hotel.Database.Database;
+import Hotel.entities.Role.Role;
+import Hotel.Service.AuthService;
+import Hotel.entities.Kard;
+import Hotel.entities.Room;
+import Hotel.entities.User;
 
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; ++i) {
+            for(int j = 0; j < 20; ++j) {
+                Room room = new Room(i + 1, j + 1, true, true);
+                Database.rooms[i][j] = room;
+            }
+        }
+
+        Kard kard = new Kard(null, null, null, 0.0);
+        User user1 = new User("user", "user", "user", kard, Role.USER);
+        User admin1 = new User("admin", "admin", "admin", kard, Role.ADMIN);
+        Database.users.add(user1);
+        Database.users.add(admin1);
+        AuthService.Menyu();
     }
 }
