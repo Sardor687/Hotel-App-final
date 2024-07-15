d5edff41ae34e21b467228fcfb4f8f5ac252998d
Sardorbek <sardorolimjonov0608@gmail.com>
2024-07-15 17:22:11 +0500

    Room and User

--- src/Hotel/entities/Room.java
+++ src/Hotel/entities/Room.java
@@ -1,4 +1,63 @@
 package Hotel.entities;
 
 public class Room {
+    private Integer floor_number;
+    private Integer room_number;
+    private Boolean active;
+    private Boolean free;
+    private final Double price=100000.0;
+
+    public Room(Integer floor_number, Integer room_number, Boolean active, Boolean free) {
+        this.floor_number = floor_number;
+        this.room_number = room_number;
+        this.active = active;
+        this.free = free;
+    }
+
+    public Integer getFloor_number() {
+        return floor_number;
+    }
+
+    public void setFloor_number(Integer floor_number) {
+        this.floor_number = floor_number;
+    }
+
+    public Integer getRoom_number() {
+        return room_number;
+    }
+
+    public void setRoom_number(Integer room_number) {
+        this.room_number = room_number;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public Boolean getFree() {
+        return free;
+    }
+
+    public void setFree(Boolean free) {
+        this.free = free;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "floor_number=" + floor_number +
+                ", room_number=" + room_number +
+                ", active=" + active +
+                ", free=" + free +
+                ", price=" + price +
+                '}';
+    }
 }
--- src/Hotel/entities/User.java
+++ src/Hotel/entities/User.java
@@ -1,4 +1,81 @@
 package Hotel.entities;
 
+import Hotel.Role.Role;
+
+import java.util.UUID;
+
 public class User {
+    private final String Id = UUID.randomUUID().toString();
+    private String name;
+    private String password;
+    private String tel_number;
+    private Kard Kard;
+    Role role;
+
+    public User(){}
+
+    public User(String name, String password, String tel_number, Hotel.entities.Kard kard, Role role) {
+        this.name = name;
+        this.password = password;
+        this.tel_number = tel_number;
+        Kard = kard;
+        this.role = role;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTel_number() {
+        return tel_number;
+    }
+
+    public void setTel_number(String tel_number) {
+        this.tel_number = tel_number;
+    }
+
+    public Hotel.entities.Kard getKard() {
+        return Kard;
+    }
+
+    public void setKard(Hotel.entities.Kard kard) {
+        Kard = kard;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "Id='" + Id + '\'' +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", tel_number='" + tel_number + '\'' +
+                ", Kard=" + Kard +
+                ", role=" + role +
+                '}';
+    }
 }
+
